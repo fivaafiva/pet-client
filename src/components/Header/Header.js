8714bb31355a5e3ee4e6edd1936e90831fd06c1e
Bernara Dzhaparova <57506492+BernaraD@users.noreply.github.com>
2021-04-01 19:05:42 -0400

    Initial

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import './Header.css'
 import {Link} from "react-router-dom";
-// import petsDateLogo from 'src/images/petsDateLogo.PNG';
+
 
 
 
 function Header() {
     return (
         <div className="header">
-            <Link to="/">
-                <img className="header__logo"
-                     src="https://drive.google.com/drive/u/0/my-drive" alt="petsApp-logo"
-                />
-
-                <h1 className="header__logo">petsLogo</h1>
+            <Link to="/" className="link">
+                <h1 className="header__logo">petsDate</h1>
             </Link>
 
 
             <div className="header__nav">
-                <Link to={"/login"}>
-                    <div
-                         className="header__option">
-                        <span className="header__optionLineTwo">Sign in</span>
-                    </div>
-                </Link>
+            {/*    <Link to={"/login"}>*/}
+            {/*        <div*/}
+            {/*             className="header__option">*/}
+            {/*            <span className="header__optionLineTwo">Sign in</span>*/}
+            {/*        </div>*/}
+            {/*    </Link>*/}
 
 
 
