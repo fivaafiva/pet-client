fe74da7fe8816e146661cad3f2481ffdef618637
indiraaridni <indiraaridni@mail.ru>
2021-03-31 22:49:08 -0400

    ndira

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <h1> INDIRAAAAAAAAA</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
